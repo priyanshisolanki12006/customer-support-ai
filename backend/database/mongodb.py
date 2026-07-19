98bd1aa5d8377f090bc32fc5a712366ed0e86b94
Priyanshi Solanki <priyanshi.solanki12006@gmail.com>
2026-07-19 21:30:20 +0530

    Debug Mongo env

--- backend/database/mongodb.py
+++ backend/database/mongodb.py
@@ -1,9 +1,10 @@
 import os
 from pymongo import MongoClient
 
-MONGO_URI = os.getenv("MONGO_URI")
-print("MONGO_URI =", os.getenv("MONGO_URI"))
-client = MongoClient(MONGO_URI)
+print("ENV VALUE:", "MONGO_URI" in os.environ)
+print("URI:", os.environ.get("MONGO_URI"))
+
+client = MongoClient(os.environ["MONGO_URI"])
 
 db = client["customer_support_ai"]
 
