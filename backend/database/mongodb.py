fa4481c2c56e9424016c2dae73c91db886bcad1b
Priyanshi Solanki <priyanshi.solanki12006@gmail.com>
2026-07-19 20:18:53 +0530

    Fix MongoDB connection

--- backend/database/mongodb.py
+++ backend/database/mongodb.py
@@ -1,21 +1,11 @@
-from pymongo import MongoClient
-from dotenv import load_dotenv
 import os
+from pymongo import MongoClient
 
-load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")
 
-client = MongoClient(
-    os.getenv("MONGO_URI")
-)
+client = MongoClient(MONGO_URI)
 
-db = client[
-    "customer_support_ai"
-]
+db = client["customer_support_ai"]
 
-chat_collection = db[
-    "chat_history"
-]
-
-user_collection = db[
-    "users"
-]
+chat_collection = db["chat_history"]
+user_collection = db["users"]
